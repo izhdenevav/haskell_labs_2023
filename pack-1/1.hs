d60503a549661681b62fba4624f3e0b0c5877c2c
izhdenevav <99416367+izhdenevav@users.noreply.github.com>
2023-09-20 13:32:50 +0700

    update solution of 1 lab

--- pack-1/1.hs
+++ pack-1/1.hs
@@ -1,3 +1,7 @@
-solveQuadratic :: Float -> Float -> Float -> (Float, Float) 
+solveQuadratic :: Float -> Float -> Float -> (Float, Float)
 
-solveQuadratic a b c = if (b * b - 4 * a * c < 0) then error"negative d" else ((-(b) - sqrt(b * b - 4 * a * c))/ (2 * a), (-(b) + sqrt(b * b - 4 * a * c))/(2 * a))
+solveQuadratic a b c = do
+      let d = b * b - 4 * a * c
+      if (d < 0) then error"negative d" 
+      else if (d == 0) then ((-(b))/(2 * a), (-(b))/(2 * a))
+      else ((-(b) - sqrt(b * b - 4 * a * c))/ (2 * a), (-(b) + sqrt(b * b - 4 * a * c))/(2 * a))
