cfb91af80bf0dada057fdbf5aa746ccde7b4ec40
izhdenevav <99416367+izhdenevav@users.noreply.github.com>
2023-09-27 12:24:28 +0700

    Update 2.hs

--- pack-1/2.hs
+++ pack-1/2.hs
@@ -9,6 +9,13 @@ sumNumbers n = if n < 1 then n else n `mod` 10 + sumNumbers(n `div` 10)
 
 --2
 
+conjectureCollatz :: Int -> [Int]
+
+conjectureCollatz n = do
+    if n == 0 then error"debil chto li"
+    else if n == 1 then []
+    else if n `mod` 2 == 0 then n `div` 2 : conjectureCollatz(n `div` 2)
+    else n * 3 + 1 : conjectureCollatz(n * 3 + 1)
 
 --3
 checkStepen :: Int -> Bool 
