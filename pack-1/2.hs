9981b238f8d89367bf67c1675655cf64bb1e5982
izhdenevav <99416367+izhdenevav@users.noreply.github.com>
2023-10-10 23:38:11 +0700

    Update 2.hs

--- pack-1/2.hs
+++ pack-1/2.hs
@@ -7,22 +7,30 @@ sumNumbers :: Int -> Int
 
 sumNumbers n = if n < 1 then n else n `mod` 10 + sumNumbers(n `div` 10)
 
--- analyseNumber :: Int -> Int -> Int 
+analyseNumber :: Int -> (Int, Int)
 
--- analyseNumber num kol sum | num < 10 = (kol) sum
---                           | otherwise = analyseNumber (num `div` 10) (kol + 1) (sum + num `mod` 10)
+analyseNumber num = analyseNumber' num (1, 0)
+              where analyseNumber' num acc | num < 10 = (fst acc, snd acc + num)
+                                           | otherwise = analyseNumber' (num `div` 10) ((fst acc + 1), (snd acc + num `mod` 10))
+
+dec2bin :: Int -> [Int]
+dec2bin x = dec2bin' x []
+        where dec2bin' x acc | x == 0 = acc
+                             | otherwise = dec2bin'(x `div` 2) (x `mod` 2 : acc)
 
 --2
 
-conjectureCollatz :: Int -> [Int]
+-- conjectureCollatz :: Int -> ([Int], Int, Int) 
+-- conjectureCollatz num | num > 0 = conjectureCollatz' num ([], 0, 0)
+--                       | otherwise = error"debil chto li"
 
-conjectureCollatz n = do
-    if n == 0 then error"debil chto li"
-    else if n == 1 then []
-    else if n `mod` 2 == 0 then n `div` 2 : conjectureCollatz(n `div` 2)
-    else n * 3 + 1 : conjectureCollatz(n * 3 + 1)
+-- conjectureCollatz' 1 acc = acc
+-- conjectureCollatz' num acc | num `mod` 2 == 0 = conjectureCollatz' (num `div` 2) ((num `div` 2 : fst acc), (snd acc + 1), (maximum fst acc))
+--                            | otherwise = conjectureCollatz' (num * 3 + 1) ((num * 3 + 1 : fst acc), (snd acc + 1), (maximum fst acc))
 
 --3
 checkStepen :: Int -> Bool 
 
-checkStepen n = if n `mod` 2 /= 0 then False else if n == 2 then True else checkStepen(n `div` 2) 
+checkStepen n | n `mod` 2 /= 0 = False
+              | n == 2 = True 
+              | otherwise = checkStepen(n `div` 2) 
