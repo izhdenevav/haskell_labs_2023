5df424e46b77809d7895e22432abac0555d0465f
izhdenevav <99416367+izhdenevav@users.noreply.github.com>
2023-10-30 21:17:49 +0700

    update 2.hs

---
+++ 2.hs
@@ -0,0 +1,37 @@
+--1
+kolNumbers :: Double -> Int
+
+kolNumbers n = if n < 1 then 1 else 1 + kolNumbers(n / 10)
+
+sumNumbers :: Int -> Int
+
+sumNumbers n = if n < 1 then n else n `mod` 10 + sumNumbers(n `div` 10)
+
+analyseNumber :: Int -> (Int, Int)
+
+analyseNumber num = analyseNumber' num (1, 0)
+              where analyseNumber' num acc | num < 10 = (fst acc, snd acc + num)
+                                           | otherwise = analyseNumber' (num `div` 10) ((fst acc + 1), (snd acc + num `mod` 10))
+
+dec2bin :: Int -> [Int]
+dec2bin x = dec2bin' x []
+        where dec2bin' x acc | x == 0 = acc
+                             | otherwise = dec2bin'(x `div` 2) (x `mod` 2 : acc)
+
+--2
+
+conjectureCollatz :: Int -> ([Int], Int, Int)
+conjectureCollatz num | num > 0 = conjectureCollatz' num [num] 0 0
+                      | otherwise = error"debil chto li"
+
+conjectureCollatz' :: Int -> [Int] -> Int -> Int -> ([Int], Int, Int)
+conjectureCollatz' 1 aсс cnt max = (aсс, cnt, max)
+conjectureCollatz' num acc cnt max | even num = conjectureCollatz' (div num 2) (acc ++ [div num 2]) (cnt + 1) (maximum acc)
+                                   | otherwise = conjectureCollatz' (num * 3 + 1) (acc ++ [num * 3 + 1]) (cnt + 1) (maximum acc)
+
+--3
+checkStepen :: Int -> Bool 
+
+checkStepen n | n `mod` 2 /= 0 = False
+              | n == 2 = True 
+              | otherwise = checkStepen(n `div` 2) 
