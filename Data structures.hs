f076e319a4af21a4fe1cf5fa9648b4384ca42a10
izhdenevav <99416367+izhdenevav@users.noreply.github.com>
2023-11-13 23:06:10 +0700

    rc MyList, oldestDog, divOfComplexNumbers

--- Data structures.hs
+++ Data structures.hs
@@ -1,3 +1,6 @@
+--import Data.List
+--import Data.String
+
 -- pack 5 ' maybe, recursion, structs '
 
 -- task 1 ' quadratic equation '
@@ -19,15 +22,18 @@ maybeTail :: [a] -> Maybe [a]
 maybeTail [] = Nothing
 maybeTail (x:xs) = Just xs
 
--- maybeInit :: [a] -> Maybe [a]
--- maybeInit [] = Nothing
--- maybeInit [x] = Just []
--- maybeInit (x:xs) = x : maybeInit xs
+maybeInit :: [a] -> Maybe [a]
+maybeInit [] = Nothing
+maybeInit (x:xs) = Just (maybeInit' xs x)
+                   where maybeInit' [] x  = []
+                         maybeInit' (x2:xs) x = x : maybeInit' xs x2
 
 -- finds first element x in lst, such that (predicate x == True)
 -- if no such element exists, returns Nothing
 maybeFind :: (a -> Bool) -> [a] -> Maybe a
-maybeFind predicate lst = undefined
+maybeFind predicate lst | predicate (head lst) = Just (head lst)
+                        | not (predicate (head lst)) && null (tail lst) = Nothing
+                        | otherwise = maybeFind predicate (tail lst)
 
 
 -- task 3 ' pattern matching with data structures '
@@ -70,36 +76,76 @@ dogs = [ Dog "Leander" 12 Male Beagle False
 
 -- dogs which are good boys
 goodBoys :: [Dog]
-goodBoys = undefined
+goodBoys = filter isGoodBoy dogs
 
--- dogs with name longer than 7 symbols
+--dogs with name longer than 7 symbols
+    --1
+lengthNameDog :: Dog -> Int
+lengthNameDog (Dog name _ _ _ _) = length name
+
 longNamedDogs :: [Dog]
-longNamedDogs = undefined
+longNamedDogs = filter ((>7) . lengthNameDog) dogs
 
--- among dogs, which is the most popular gender?
+    --2
+-- r = filter (\dog -> (length $ name dog) > 7) dogs
+-- r1 = filter (\(Dog name _ _ _ _) -> length name > 7) dogs
+-- r2 = filter (/dog{name} -> length name dog > 7) dogs
+
+--among dogs, which is the most popular gender?
+
+countFemaleDogs :: Int
+countFemaleDogs = length (filter (\(Dog _ _ gender _ _) -> gender == Female) dogs)
+
 mostPopularDogGender :: Gender
-mostPopularDogGender = undefined
+mostPopularDogGender | length (filter (\(Dog _ _ gender _ _) -> gender == Female) dogs) > length (filter (\(Dog _ _ gender _ _) -> gender == Male) dogs) = Female
+                     | otherwise = Male
 
-oldestDog :: Dog
-oldestDog = undefined
+--oldest dog
+--oldestDog :: Dog
+--oldestDog = (\(Dog _ age _ _ _) acc -> )
 
 averageDogAge :: Double
-averageDogAge = undefined
+averageDogAge = fromIntegral (foldl (\ acc (Dog _ age _ _ _) -> acc + age) 0 dogs) / fromIntegral (length dogs)
 
 -- finds dogs with given breed
 dogsByBreed :: DogBreed -> [Dog]
-dogsByBreed = undefined
+dogsByBreed dogBreed = filter (\(Dog _ _ _ breed _) -> breed == dogBreed) dogs
 
 ----- data structures
 -- task 4.1
 
 -- Создайте тип, который реализует комплексные числа
--- создайте функции, которые реализуют:
--- - сумму, разницу
--- - умножение, деление
--- - взятие сопряженного
--- - взятие абсолютного значения
+data ComplexNumber = ComplexNumber { a :: Double
+                                   , b :: Double
+                                   } deriving (Show, Eq)
 
+cn1 = ComplexNumber {a = 3.0, b = 1.0}
+
+cn2 = ComplexNumber {a = 5.0, b = -2.0}
+
+-- создайте функции, которые реализуют:
+
+-- - сумму, разницу
+sumOfComplexNumbers :: ComplexNumber
+sumOfComplexNumbers = ComplexNumber {a = a cn1 + a cn2, b = b cn1 + b cn2}
+
+subOfComplexNumbers :: ComplexNumber
+subOfComplexNumbers = ComplexNumber {a = a cn1 - a cn2, b = b cn1 - b cn2}
+
+-- - умножение, деление
+multipOfComplexNumbers :: ComplexNumber
+multipOfComplexNumbers = ComplexNumber {a = a cn1 * a cn2 + b cn1 * b cn2 * (-1), b = a cn1 * b cn2 + b cn1 * a cn2}
+
+divOfComplexNumbers :: ComplexNumber
+divOfComplexNumbers = undefined
+
+-- - взятие сопряженного
+conjugateComplexNumber :: ComplexNumber
+conjugateComplexNumber = ComplexNumber {a = a cn1, b = -b cn1}
+
+-- - взятие абсолютного значения
+absoluteValue :: Double
+absoluteValue = sqrt(a cn1**2 + b cn1**2) 
 
 -- Создайте тип, который образует односвязный список (<=> список имеет голову и хвост, либо является пустым)
 -- реализуйте  для него следующие методы:
