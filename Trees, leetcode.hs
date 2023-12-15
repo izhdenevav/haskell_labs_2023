dc3108e03531ddea529d19326b9f9f05a390c265
izhdenevav <99416367+izhdenevav@users.noreply.github.com>
2023-12-15 23:22:49 +0700

    update: ++full tree, ++hald of deck tasks, ++matrix, ++full ipv4, ++nice numbers???

--- Trees, leetcode.hs
+++ Trees, leetcode.hs
@@ -1,4 +1,7 @@
 {-# LANGUAGE DeriveFoldable #-}
+import Data.Maybe
+import Data.Char
+import qualified Data.Text as T
 
 --trees
 data Tree a = Empty
@@ -13,31 +16,45 @@ tree1 = Node Empty Nothing (Node (Node (Leaf Nothing) Nothing (Node Empty Nothin
 
 --a
 findCat :: Tree (Maybe Cat) -> Int
-findCat tree = findCat' 0 tree
-               where findCat' acc (Just Cat) = acc + 1
-                     findCat' acc (Leaf (Just Cat)) = acc + 1
-                     findCat' acc (Leaf Nothing) = acc
-                     findCat' acc (Node left _ right) = acc + max (findCat' acc left) (findCat' acc right)
-                     
+findCat tree = length (fromJust (wayToCat tree)) + 1
+
 --b
-wayToCat :: Tree (Maybe Cat) -> String
-wayToCat tree = undefined
+wayToCat :: Tree (Maybe Cat) -> Maybe [String]
+wayToCat Empty = Nothing
+wayToCat (Leaf Nothing) = Nothing
+wayToCat (Leaf (Just Cat)) = Just []
+wayToCat (Node left _ right) = case (wayToCat left, wayToCat right) of
+            (Just path, _) -> Just ("l" : path)
+            (_, Just path) -> Just ("r" : path)
+            _ -> Nothing
 
---чзх
+--casino
 
-data Suit = Hearts | Tiles | Clovers | Pikes
+data Suit = Hearts | Tiles | Clovers | Pikes deriving (Show, Eq, Ord)
 type Value = Int
-data Card = JokerA | JokerB | Card Suit Value
+data Card = JokerA | JokerB | Card Suit Value deriving (Show, Eq, Ord)
 
 --Card Pikes (-1)
 
+deck1 :: [Card] 
+deck1 = [JokerA, JokerB] ++ [Card Hearts x | x <- [1..13]]
+
+fullDeck :: [Card]
+fullDeck = [JokerA, JokerB] ++ [Card Hearts x | x <- [1..13]] ++ [Card Tiles x | x <- [1..13]] ++ [Card Clovers x | x <- [1..13]] ++ [Card Pikes x | x <- [1..13]]
+
 --a
 isDeckFull :: [Card] -> Bool
-isDeckFull deck = undefined 
+isDeckFull deck | length deck /= 54 = False
+                | otherwise = all (`elem` deck) fullDeck
 
 --a.2
-isOrderRight :: [Card] -> Bool
-isOrderRight deck = undefined
+-- isOrderRight :: [Card] -> Bool
+-- isOrderRight deck | not (isDeckFull deck) = False
+--                   | otherwise = isOrderRight' deck 
+--                                 where isOrderRight' :: [Card] -> Bool
+--                                       isOrderRight' (Card suit1 rank1 : Card suit2 rank2 : ost)  | suit1 < suit2 = True
+--                                                                                                      | suit1 == suit2 && rank1 < rank2 = True
+--                                                                                                      | otherwise = False && isOrderRight' (Card suit2 rank2 : ost)
 
 --b
 --chzh
@@ -46,37 +63,73 @@ isOrderRight deck = undefined
 
 --a
 powerset :: [a] -> [[a]]
-powerset set = undefined
+powerset [] = [[]]
+powerset (x:xs) = map (x:) (powerset xs) ++ powerset xs 
 
 --b
-IpAdress1 :: String -> Bool
-IpAdress1 str = undefined
+splitString :: String -> [T.Text]
+splitString str = T.splitOn (T.pack ".") (T.pack str)
 
+symbolToInteger :: Char -> Int
+symbolToInteger sym | ord sym > 64 && ord sym < 91 = ord sym - 55
+                    | ord sym > 96 && ord sym < 123 = ord sym - 87
+                    | ord sym > 47 && ord sym < 58 = ord sym - 48
+                    | otherwise = error"патамушта понабирают всяких"
+                    
+stringToInt :: String -> Int
+stringToInt str = foldl (\ acc c -> (acc + symbolToInteger c) * 10) 0 (init str) + symbolToInteger (last str)
+
+isSegmentValid :: T.Text -> Bool
+isSegmentValid segment | length (T.unpack segment) <= 3 && all (isDigit) (T.unpack segment) && stringToInt (T.unpack segment) <= 255 && stringToInt (T.unpack segment) >= 0 = True
+                       | otherwise = False
+
+validIPV4 :: String -> Bool
+validIPV4 str | all (isSegmentValid) (splitString str) && length (splitString str) == 4 = True
+              | otherwise = False
 --b.2
-IpAdress2 :: String -> [String]
-IpAdress2 str = undefined
+generateIPV4 :: String -> Int -> [String]
+generateIPV4 _ 0 = []
+generateIPV4 [] _ = []
+generateIPV4 [x] _ = [[x]]
+generateIPV4 (x:xs) n = map ((x: ".") ++) (generateIPV4 xs (n - 1)) ++ map (x: ) (generateIPV4 xs n)
+
+generateValidIPV4 :: String -> [String]
+generateValidIPV4 str = filter validIPV4 $ generateIPV4 str 4
 
 --c
-Histogram :: [Int] -> [String]
-Histogram = undefined
+aCount :: (Eq a) => [a] -> a -> Int
+aCount list el = (length . filter (== el)) list
+
+--makeHistogram :: (Eq a, Show a) => [a] -> String
+--makeHistogram list = "Total: " ++ show (length list) ++ " elem" ++ "\n" ++ 
+
+--map (\el -> el ++ " " ++ show (aCount list el) list
 
 --d
-Matrix :: [[Int]] -> [[Int]]
-Metrix mtrx = undefined
+myMatrix :: [[Int]] -> [[Int]]
+myMatrix ([]:_) = []
+myMatrix mtrx = (map head mtrx) : myMatrix (map tail mtrx)
 
 --e
---chzh
+sumQuadr :: Int -> Int
+sumQuadr num | num < 1 = num 
+             | otherwise = (num `mod` 10)^2 + sumQuadr(num `div` 10)
+
+niceNumber :: Int -> Bool
+niceNumber num | num == 1 = True
+               | sumQuadr num == 1 = True
+               | otherwise = False
 
 --advanced
 
 --a
-tic-tac-toe :: [[Int]] -> Maybe([[Int]], String)
-tic-tac-toe field = undefined
+-- tic-tac-toe :: [[Int]] -> Maybe([[Int]], String)
+-- tic-tac-toe field = undefined
 
 --b
-permutations :: [Int] -> [[Int]]
-permutations = undefined
+-- permutations :: [Int] -> [[Int]]
+-- permutations = undefined
 
 --c
-pi :: Int -> Double
-pi = undefined
+-- pi :: Int -> Double
+-- pi = undefined
